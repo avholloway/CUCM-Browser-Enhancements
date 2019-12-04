82d05fd36ca5a8985a7de7309470df21a600d002
Anthony Holloway <avholloway@gmail.com>
2019-12-04 17:46:18 -0600

    added row getter with 3 column filter limiters

--- enterprise-and-service-parameter-highlighting.user.js
+++ enterprise-and-service-parameter-highlighting.user.js
@@ -13,4 +13,7 @@
 (function() {
   'use strict';
   console.log(`location: ${window.location.pathname}`);
+
+  // Grab all rows on the page which have exactly three columns (param name, param value, default value)
+  const rows = $('tr').filter(function(index) { return $('td', this).length === 3});
 })();
