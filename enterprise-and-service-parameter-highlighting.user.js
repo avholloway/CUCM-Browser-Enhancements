fea7b4265c5db34fe044f678da3ec3a5272e79eb
Anthony Holloway <avholloway@gmail.com>
2019-12-04 17:33:16 -0600

    added ent/svc param highligting file

---
+++ enterprise-and-service-parameter-highlighting.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         CUCM Enterprise Parameter Highlighting
+// @namespace    http://avholloway.com/
+// @version      0.1
+// @description  Enhance your CUCM browsing
+// @author       Anthony Holloway (avholloway@gmail.com)
+// @match        https://*/ccmadmin/enterpriseParamEdit.do*
+// @match        https://*/ccmadmin/serviceParamEdit.do*
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  console.log(`location: ${window.location.pathname}`);
+})();
